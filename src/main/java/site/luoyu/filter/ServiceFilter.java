3851cc778c246b36e6824dc088789f90c557994c
luoyu <z_y_ang@163.com>
2016-09-17 16:27:01 +0800

    add croess origin access

---
+++ src/main/java/site/luoyu/filter/ServiceFilter.java
@@ -0,0 +1,29 @@
+package site.luoyu.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by xd on 2016/9/17.
+ */
+public class ServiceFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "X-Requested-With");
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
