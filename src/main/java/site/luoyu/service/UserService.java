2a1a1878b125d7bbf881c729aa3fdfef558acd97
ProTaoXu <18792823629@163.com>
2016-10-18 10:30:00 +0800

    Update UserService.java

--- src/main/java/site/luoyu/service/UserService.java
+++ src/main/java/site/luoyu/service/UserService.java
@@ -65,5 +65,10 @@ public class UserService {
      * 注册服务
      */
     public void updateInfo(User user){
+        UserStudent userStudent =userStudentRepository.findByStuId(user.getStuId());
+    	userStudent.setName(user.getName());
+    	userStudent.setPasswd(user.getPasswd());
+    	userStudentRepository.save(userStudent);
+
     }
 }
